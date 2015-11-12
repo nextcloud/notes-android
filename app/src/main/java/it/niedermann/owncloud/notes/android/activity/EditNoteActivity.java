c77c9cf6f09386381330ccae5f0710909e65c2bd
Stefan Niedermann <stefan.niedermann@googlemail.com>
2015-11-12 21:00:32 +0100

    WiP #26 Autosave

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/EditNoteActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/EditNoteActivity.java
@@ -2,64 +2,95 @@ package it.niedermann.owncloud.notes.android.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import it.niedermann.owncloud.notes.R;
 import it.niedermann.owncloud.notes.model.Note;
 import it.niedermann.owncloud.notes.persistence.NoteSQLiteOpenHelper;
 
 public class EditNoteActivity extends AppCompatActivity {
+	private final long DELAY = 1000; // in ms
 	private EditText content = null;
 	private Note note = null;
+	private Timer timer = new Timer();
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
         note = (Note) getIntent().getSerializableExtra(
 				NoteActivity.EDIT_NOTE);
 		content = (EditText) findViewById(R.id.editContent);
-        content.setEnabled(false);
-        content.setText(note.getContent());
-        content.setSelection(note.getContent().length());
-        content.setEnabled(true);
+		content.setEnabled(false);
+		content.setText(note.getContent());
+		content.setEnabled(true);
+		content.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+										  int after) {
+			}
+
+			@Override
+			public void onTextChanged(final CharSequence s, int start, int before,
+									  int count) {
+				if (timer != null)
+					timer.cancel();
+			}
+
+			@Override
+			public void afterTextChanged(final Editable s) {
+				timer = new Timer();
+				timer.schedule(new TimerTask() {
+					@Override
+					public void run() {
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								saveData();
+							}
+						});
+					}
+				}, DELAY);
+			}
+		});
 	}
 
-	/**
-	 * Create Action Menu
-	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_menu_edit, menu);
-		return true;
+	public void onBackPressed() {
+		content.setEnabled(false);
+		saveData();
+		Intent data = new Intent();
+		data.setAction(Intent.ACTION_VIEW);
+		data.putExtra(NoteActivity.EDIT_NOTE, note);
+		setResult(RESULT_OK, data);
+		finish();
 	}
 
-	/**
-	 * Handle Action Menu
-	 */
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		switch (id) {
-		case R.id.action_edit_save:
-			content.setEnabled(false);
-            note.setContent(((EditText) findViewById(R.id.editContent)).getText().toString());
-            NoteSQLiteOpenHelper db = new NoteSQLiteOpenHelper(this);
-            db.updateNoteAndSync(note);
-            Intent data = new Intent();
-            data.setAction(Intent.ACTION_VIEW);
-            data.putExtra(NoteActivity.EDIT_NOTE, note);
-            setResult(RESULT_OK, data);
-            finish();
-			return true;
-            case R.id.action_edit_cancel:
-            finish();
-            return true;
-		default:
-			return super.onOptionsItemSelected(item);
+	private void saveData() {
+		ActionBar ab = getSupportActionBar();
+		if (ab != null) {
+			ab.setSubtitle(getResources().getString(R.string.action_edit_saving));
 		}
+		note.setContent(((EditText) findViewById(R.id.editContent)).getText().toString());
+		NoteSQLiteOpenHelper db = new NoteSQLiteOpenHelper(this);
+		db.updateNoteAndSync(note);
+		if (ab != null) {
+			getSupportActionBar().setSubtitle(getResources().getString(R.string.action_edit_saved));
+		}
+		Executors.newSingleThreadScheduledExecutor().schedule(new Runnable() {
+			@Override
+			public void run() {
+				getSupportActionBar().setSubtitle(null);
+			}
+		}, 1, TimeUnit.SECONDS);
 	}
 }
