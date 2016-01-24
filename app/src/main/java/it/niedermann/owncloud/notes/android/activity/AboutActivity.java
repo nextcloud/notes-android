353949760bb22375eae7613f30e0fb6bcd982130
Stefan Niedermann <stefan.niedermann@googlemail.com>
2016-01-24 10:31:15 +0100

    WiP #40 Mention license, source code and call to contribution in the »About« section - Switched to Tabbed Layout in About Activity

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/AboutActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/AboutActivity.java
@@ -1,15 +1,31 @@
 package it.niedermann.owncloud.notes.android.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 
 import it.niedermann.owncloud.notes.R;
+import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentContributingTab;
+import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentCredentialsTab;
+import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentLicenseTab;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private FragmentTabHost mTabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_about);
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Credentials").setIndicator("Credentials"),
+                AboutFragmentCredentialsTab.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Contribution").setIndicator("Contribution"),
+                AboutFragmentContributingTab.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("License").setIndicator("License"),
+                AboutFragmentLicenseTab.class, null);
     }
+
 }
---
+++ app/src/main/java/it/niedermann/owncloud/notes/android/fragment/about/AboutFragmentLicenseTab.java
@@ -0,0 +1,16 @@
+package it.niedermann.owncloud.notes.android.fragment.about;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.niedermann.owncloud.notes.R;
+
+public class AboutFragmentLicenseTab extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about_license_tab, container, false);
+    }
+}
