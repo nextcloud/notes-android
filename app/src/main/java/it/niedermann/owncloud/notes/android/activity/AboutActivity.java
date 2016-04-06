2a76a37837c10baf97026c08a621ac943b6d700e
Felix Edelmann <fxedel@gmail.com>
2016-04-06 15:28:08 +0200

    Use TabLayout and ViewPager for about fragments; closes #85

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/AboutActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/AboutActivity.java
@@ -1,30 +1,78 @@
 package it.niedermann.owncloud.notes.android.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import it.niedermann.owncloud.notes.R;
-import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentContributingTab;
-import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentCreditsTab;
-import it.niedermann.owncloud.notes.android.fragment.about.AboutFragmentLicenseTab;
+import it.niedermann.owncloud.notes.android.fragment.about.*;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private FragmentTabHost mTabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_about);
-        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
 
-        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.about_credits_tab_title)).setIndicator(getString(R.string.about_credits_tab_title)),
-                AboutFragmentCreditsTab.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.about_contribution_tab_title)).setIndicator(getString(R.string.about_contribution_tab_title)),
-                AboutFragmentContributingTab.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.about_license_tab_title)).setIndicator(getString(R.string.about_license_tab_title)),
-                AboutFragmentLicenseTab.class, null);
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        TabLayout mTabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    private class TabsPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+
+        public TabsPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        // return the right fragment for the given position
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new AboutFragmentCreditsTab();
+
+                case 1:
+                    return new AboutFragmentContributingTab();
+
+                case 2:
+                    return new AboutFragmentLicenseTab();
+
+                default:
+                    return null;
+            }
+        }
+
+        // generate title based on given position
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.about_credits_tab_title);
+
+                case 1:
+                    return getString(R.string.about_contribution_tab_title);
+
+                case 2:
+                    return getString(R.string.about_license_tab_title);
+
+                default:
+                    return null;
+            }
+        }
     }
 }
