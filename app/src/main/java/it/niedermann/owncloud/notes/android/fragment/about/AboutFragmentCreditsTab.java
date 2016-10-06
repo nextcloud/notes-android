054000631c6ee1f382676a6a361c02c2de6410cf
korelstar <github@korelstar.de>
2016-10-06 10:53:16 +0200

    Load version name dynamically from package (#139)

--- app/src/main/java/it/niedermann/owncloud/notes/android/fragment/about/AboutFragmentCreditsTab.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/fragment/about/AboutFragmentCreditsTab.java
@@ -1,5 +1,6 @@
 package it.niedermann.owncloud.notes.android.fragment.about;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.method.LinkMovementMethod;
@@ -15,6 +16,13 @@ public class AboutFragmentCreditsTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about_credits_tab, container, false);
         ((TextView) v.findViewById(R.id.about_maintainer)).setMovementMethod(LinkMovementMethod.getInstance());
+        String versionName;
+        try {
+            versionName = "v"+getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            versionName = "(error)";
+        }
+        ((TextView) v.findViewById(R.id.about_version)).setText(getString(R.string.about_version, versionName));
         return v;
     }
 }
