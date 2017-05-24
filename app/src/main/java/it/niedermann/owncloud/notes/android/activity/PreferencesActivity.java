8ec387acea911595f2b85ca0da31c267cdb99398
schaarsc <schaarsc>
2017-05-24 20:33:07 +0200

    align L&F with other activities.

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/PreferencesActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/PreferencesActivity.java
@@ -1,8 +1,8 @@
 package it.niedermann.owncloud.notes.android.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import it.niedermann.owncloud.notes.android.fragment.PreferencesFragment;
 
@@ -10,7 +10,7 @@ import it.niedermann.owncloud.notes.android.fragment.PreferencesFragment;
  * Allows to change application settings.
  */
 
-public class PreferencesActivity extends Activity {
+public class PreferencesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
