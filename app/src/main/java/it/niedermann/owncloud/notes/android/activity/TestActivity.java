9a9acbfd7a703059f8af5c9ccdc1071604aeff19
Daniel Bailey <dan0xii@users.github.com>
2017-08-20 20:58:53 +0100

    Finished note selection window

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/TestActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/TestActivity.java
@@ -1,6 +1,11 @@
 package it.niedermann.owncloud.notes.android.activity;
 
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +16,7 @@ import it.niedermann.owncloud.notes.R;
 import it.niedermann.owncloud.notes.model.DBNote;
 import it.niedermann.owncloud.notes.model.Item;
 import it.niedermann.owncloud.notes.model.ItemAdapter;
+import it.niedermann.owncloud.notes.persistence.NoteSQLiteOpenHelper;
 
 import static android.R.string.cancel;
 
@@ -20,11 +26,18 @@ import static android.R.string.cancel;
 
 public class TestActivity extends NotesListViewActivity {
 
+    private NoteSQLiteOpenHelper db = null;
+    private ItemAdapter adapter = null;
+
     private static final String TAG = TestActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db = NoteSQLiteOpenHelper.getInstance(this);
+
+        this.setResult(Activity.RESULT_CANCELED);
 
 //        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
  //       setContentView(R.layout.activity_notes_list_view);
@@ -32,8 +45,10 @@ public class TestActivity extends NotesListViewActivity {
    //     getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.activity_select_single_note);
 
         findViewById(R.id.fab_create).setVisibility(View.INVISIBLE);
-
         getSupportActionBar().setTitle(R.string.activity_select_single_note);
+
+        initList();
+        adapter = getItemAdapter();
     }
 
     @Override
@@ -42,11 +57,43 @@ public class TestActivity extends NotesListViewActivity {
     @Override
     public void onNoteClick(int position, View v) {
 
-        ItemAdapter adapter = new ItemAdapter(this);
+        // ItemAdapter adapter = new ItemAdapter(this);
 
-        Log.d(TAG, "onNoteClick: " + position);
+        Log.d(TAG, "onNoteClick: Position: " + position);
         Item item = adapter.getItem(position);
-        Log.d(TAG, "onNoteClick: " + (DBNote) item);
+        DBNote note = (DBNote) item;
+        long noteID = note.getId();
+        Log.d(TAG, "onNoteClick: noteID: " + noteID);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        int mAppWidgetId = 0;
+
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
+
+        Log.d("SelectSingleNote", "mAppWidgetId: " + mAppWidgetId);
+
+        SharedPreferences.Editor sp = PreferenceManager.getDefaultSharedPreferences(this).edit();
+
+        Log.d("SelectSingleNote", "Adding noteID (" + noteID + ") to sharedPrefs");
+        // TODO Add widgetID to this key
+        sp.putLong(SingleNoteWidget.WIDGET_KEY + mAppWidgetId, noteID);
+        sp.putBoolean(SingleNoteWidget.WIDGET_KEY + mAppWidgetId + SingleNoteWidget.INIT, true);
+        sp.apply();
+
+
+        Intent retIntent = new Intent(this, SingleNoteWidget.class);
+        retIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, mAppWidgetId);
+        retIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        this.sendBroadcast(retIntent);
+        this.setResult(RESULT_OK, retIntent);
+
+        Log.d(TAG, "onNoteClick: Sent return intent, RESULT_OK");
+        finish();
     }
 
     @Override
