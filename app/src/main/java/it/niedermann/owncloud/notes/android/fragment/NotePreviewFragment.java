467d2e578bf587c5cc75c26aa347323b26498b33
schaarsc <schaarsc>
2017-08-30 19:48:58 +0200

    Merge Edit & Preview into one activity.

--- app/src/main/java/it/niedermann/owncloud/notes/android/fragment/NotePreviewFragment.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/fragment/NotePreviewFragment.java
@@ -1,13 +1,17 @@
-package it.niedermann.owncloud.notes.android.activity;
+package it.niedermann.owncloud.notes.android.fragment;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateUtils;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.yydcdut.rxmarkdown.RxMDTextView;
@@ -15,33 +19,61 @@ import com.yydcdut.rxmarkdown.RxMarkdown;
 import com.yydcdut.rxmarkdown.factory.TextFactory;
 
 import it.niedermann.owncloud.notes.R;
+import it.niedermann.owncloud.notes.android.activity.EditNoteActivity;
 import it.niedermann.owncloud.notes.model.DBNote;
 import it.niedermann.owncloud.notes.util.MarkDownUtil;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class NoteActivity extends AppCompatActivity {
+public class NotePreviewFragment extends Fragment implements NoteFragmentI {
 
     public static final String PARAM_NOTE = EditNoteActivity.PARAM_NOTE;
 
     private DBNote note = null;
-    private RxMDTextView noteContent = null;
+
+    public static NotePreviewFragment newInstance(DBNote note) {
+        NotePreviewFragment f = new NotePreviewFragment();
+        Bundle b = new Bundle();
+        b.putSerializable(PARAM_NOTE, note);
+        f.setArguments(b);
+        return f;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public DBNote getNote() {
+        return note;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_note);
-        note = (DBNote) getIntent().getSerializableExtra(PARAM_NOTE);
-        if (savedInstanceState != null) {
-            note = (DBNote) savedInstanceState.getSerializable(PARAM_NOTE);
-        }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(note.getTitle());
-            actionBar.setSubtitle(DateUtils.getRelativeDateTimeString(getApplicationContext(), note.getModified().getTimeInMillis(), DateUtils.MINUTE_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, 0));
-        }
-        noteContent = (RxMDTextView) findViewById(R.id.single_note_content);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        MenuItem itemPreview = menu.findItem(R.id.menu_preview);
+        itemPreview.setIcon(ContextCompat.getDrawable(getActivity(), R.drawable.ic_action_edit_24dp));
+        MenuItem itemCategory = menu.findItem(R.id.menu_category);
+        itemCategory.setVisible(false);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_single_note, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        note = (DBNote) getArguments().getSerializable(PARAM_NOTE);
+        final RxMDTextView noteContent = (RxMDTextView) getActivity().findViewById(R.id.single_note_content);
 
         String content = note.getContent();
         /*
@@ -58,8 +90,8 @@ public class NoteActivity extends AppCompatActivity {
          */
         content = content.replaceAll("(?<![(])(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])(?![^\\[]*\\])", "[$1]($1)");
 
-        RxMarkdown.with(content, this)
-                .config(MarkDownUtil.getMarkDownConfiguration(getApplicationContext()))
+        RxMarkdown.with(content, getActivity())
+                .config(MarkDownUtil.getMarkDownConfiguration(getActivity().getApplicationContext()))
                 .factory(TextFactory.create())
                 .intoObservable()
                 .subscribeOn(Schedulers.computation())
@@ -80,24 +112,7 @@ public class NoteActivity extends AppCompatActivity {
                     }
                 });
         noteContent.setText(content);
-        findViewById(R.id.fab_edit).setVisibility(View.GONE);
-        ((TextView) findViewById(R.id.single_note_content)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) getActivity().findViewById(R.id.single_note_content)).setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putSerializable(PARAM_NOTE, note);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
