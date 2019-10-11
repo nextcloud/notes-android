b4a83475024a38acdbfbc60dc0500f030a6e2989
stefan-niedermann <info@niedermann.it>
2019-10-11 13:51:51 +0200

    Fix account import for Android 4 (and maybe others)

--- app/src/main/java/it/niedermann/owncloud/notes/util/SSOUtil.java
+++ app/src/main/java/it/niedermann/owncloud/notes/util/SSOUtil.java
@@ -6,6 +6,7 @@ import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -20,6 +21,8 @@ import com.nextcloud.android.sso.ui.UiExceptionManager;
 import static com.nextcloud.android.sso.AccountImporter.CHOOSE_ACCOUNT_SSO;
 
 public class SSOUtil {
+
+    private static final String TAG = SSOUtil.class.getSimpleName();
 
     private SSOUtil() {
 
@@ -47,27 +50,29 @@ public class SSOUtil {
     /**
      * Opens the same dialog like AccountImporter.pickNewAccount() but preselects the given account
      *
-     * // FIXME does not work on Android 4 (and maybe others) yet
-     *
      * @param activity should implement CHOOSE_ACCOUNT_SSO in onActivityResult
      * @param accountName account that should be preselected
      */
     public static void authorizeExistingAccount(@NonNull Activity activity, @NonNull String accountName) {
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(SSOUtil.class.getSimpleName(), "Permission granted!");
-            Intent intent = AccountManager.newChooseAccountIntent(
-                    new Account(accountName, Constants.ACCOUNT_TYPE_PROD),
-                    null,
-                    new String[]{Constants.ACCOUNT_TYPE_PROD, Constants.ACCOUNT_TYPE_DEV},
-                    true,
-                    null,
-                    null,
-                    null,
-                    null
-            );
-            activity.startActivityForResult(intent, CHOOSE_ACCOUNT_SSO);
-        } else {
-            AccountImporter.requestAndroidAccountPermissionsAndPickAccount(activity);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+                Log.w(TAG, "Permission not granted.");
+                // Well... do you want to use this SSO account or not?
+                return;
+            }
         }
+
+        Log.d(TAG, "Permission granted.");
+        Intent intent = AccountManager.newChooseAccountIntent(
+                new Account(accountName, Constants.ACCOUNT_TYPE_PROD),
+                null,
+                new String[]{Constants.ACCOUNT_TYPE_PROD, Constants.ACCOUNT_TYPE_DEV},
+                true,
+                null,
+                null,
+                null,
+                null
+        );
+        activity.startActivityForResult(intent, CHOOSE_ACCOUNT_SSO);
     }
 }
