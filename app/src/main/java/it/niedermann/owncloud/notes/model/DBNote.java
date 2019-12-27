02e4976a02c0e3c1ab1d832894b49c68fd16e483
stefan-niedermann <info@niedermann.it>
2019-12-27 09:41:03 +0100

    #528 Save the excerpt in a separate column

--- app/src/main/java/it/niedermann/owncloud/notes/model/DBNote.java
+++ app/src/main/java/it/niedermann/owncloud/notes/model/DBNote.java
@@ -5,8 +5,6 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 import java.util.Calendar;
 
-import it.niedermann.owncloud.notes.util.NoteUtil;
-
 /**
  * DBNote represents a single note from the local SQLite database with all attributes.
  * It extends CloudNote with attributes required for local data management.
@@ -16,12 +14,12 @@ public class DBNote extends CloudNote implements Item, Serializable {
     private long id;
     private long accountId;
     private DBStatus status;
-    private String excerpt = "";
+    private String excerpt;
 
-    public DBNote(long id, long remoteId, Calendar modified, String title, String content, boolean favorite, String category, String etag, DBStatus status, long accountId) {
+    public DBNote(long id, long remoteId, Calendar modified, String title, String content, boolean favorite, String category, String etag, DBStatus status, long accountId, String excerpt) {
         super(remoteId, modified, title, content, favorite, category, etag);
         this.id = id;
-        setExcerpt(content);
+        this.excerpt = excerpt;
         this.status = status;
         this.accountId = accountId;
     }
@@ -46,17 +44,12 @@ public class DBNote extends CloudNote implements Item, Serializable {
         return excerpt;
     }
 
-    public void setExcerptDirectly(String content) {
-        excerpt = content;
-    }
-
-    private void setExcerpt(String content) {
-        excerpt = NoteUtil.generateNoteExcerpt(content);
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
     }
 
     public void setContent(String content) {
         super.setContent(content);
-        setExcerpt(content);
     }
 
     @Override
