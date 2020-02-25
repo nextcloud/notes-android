5b288b6fbb542d6dc0997d1d00d8500a22cd509e
Christoph Loy <loy.christoph@gmail.com>
2020-02-25 17:39:35 +0100

    Remove Butterknife from PreferencesActivity

--- app/src/main/java/it/niedermann/owncloud/notes/android/activity/PreferencesActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/android/activity/PreferencesActivity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import it.niedermann.nextcloud.exception.ExceptionHandler;
 import it.niedermann.owncloud.notes.R;
 import it.niedermann.owncloud.notes.android.fragment.PreferencesFragment;
+import it.niedermann.owncloud.notes.databinding.ActivityPreferencesBinding;
 
 /**
  * Allows to change application settings.
@@ -18,16 +16,13 @@ import it.niedermann.owncloud.notes.android.fragment.PreferencesFragment;
 
 public class PreferencesActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Thread.currentThread().setUncaughtExceptionHandler(new ExceptionHandler(this));
-        setContentView(R.layout.activity_preferences);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+        ActivityPreferencesBinding binding = ActivityPreferencesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
         setResult(RESULT_CANCELED);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container_view, new PreferencesFragment())
