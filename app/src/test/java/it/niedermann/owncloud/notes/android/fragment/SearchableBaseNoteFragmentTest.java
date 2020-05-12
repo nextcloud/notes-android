81389d847ac129bc389902e4880b3d65f2688d31
Isc <ccccym666@gmail.com>
2020-05-12 18:26:31 +0800

    Remove random test for fixed test is good enough

--- app/src/test/java/it/niedermann/owncloud/notes/android/fragment/SearchableBaseNoteFragmentTest.java
+++ app/src/test/java/it/niedermann/owncloud/notes/android/fragment/SearchableBaseNoteFragmentTest.java
@@ -45,42 +45,6 @@ public class SearchableBaseNoteFragmentTest {
         }
     }
 
-//    @Test
-//    public void testCountOccurrencesRandom() {
-//        try {
-//            Method method = SearchableBaseNoteFragment.class.getDeclaredMethod("countOccurrences", String.class, String.class);
-//            method.setAccessible(true);
-//
-//            for (int count = 10; count <= 15; ++count) {
-//                StringBuilder sb = new StringBuilder("Mike Chester Wang");
-//                Random rand = new Random();
-//                for (int i = 0; i < count * 100; ++i) {
-//                    sb.append(rand.nextDouble());
-//                    if (i % 100 == 0) {
-//                        sb.append("flag");
-//                    }
-//                }
-//
-//                long startTime = System.currentTimeMillis();
-//                int num = (int) method.invoke(null, sb.toString(), String.valueOf(rand.nextInt(100)));
-//                long endTime = System.currentTimeMillis();
-//                System.out.println("Random Version");
-//                System.out.println("Total Time: " + (endTime - startTime) + " ms");
-//                System.out.println("Total Times: " + num);
-//                System.out.println("String Size: " + (sb.length() / 1024) + " K");
-//                System.out.println();
-//
-//                if (endTime - startTime > 10) {
-//                    fail("The algorithm spends too much time.");
-//                }
-//            }
-//
-//        } catch (Exception e) {
-//            fail(Arrays.toString(e.getStackTrace()));
-//            Log.e("Test Count Occurrences Random", Arrays.toString(e.getStackTrace()));
-//        }
-//    }
-
     @Test
     public void testNullOrEmptyInput() {
         try {
