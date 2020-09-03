242a6983af4dd717c467287c63db7a0b34cf789f
Stefan Niedermann <info@niedermann.it>
2020-09-03 19:44:09 +0200

    #949 Support www links without protocol in preview mode

--- app/src/main/java/it/niedermann/owncloud/notes/shared/util/MarkDownUtil.java
+++ app/src/main/java/it/niedermann/owncloud/notes/shared/util/MarkDownUtil.java
@@ -88,6 +88,9 @@ public class MarkDownUtil {
             text = TextUtils.replace(text, MD_IMAGE_WITH_EMPTY_DESCRIPTION_ARRAY, MD_IMAGE_WITH_SPACE_DESCRIPTION_ARRAY);
         }
 
+        // https://github.com/stefan-niedermann/nextcloud-notes/issues/949
+        text = text.toString().replaceAll("(?<![(])(www.[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])(?![^\\[]*])", "[$1]($1)");
+
         return markdownProcessor.parse(text);
     }
 
