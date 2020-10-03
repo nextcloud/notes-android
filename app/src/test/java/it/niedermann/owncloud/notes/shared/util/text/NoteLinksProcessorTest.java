8daed49c04e31495b719b309b8be428c7e99f6ee
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-10-03 13:04:30 +0200

    support www links without protocol in preview mode (resolve #949)

--- app/src/test/java/it/niedermann/owncloud/notes/shared/util/text/NoteLinksProcessorTest.java
+++ app/src/test/java/it/niedermann/owncloud/notes/shared/util/text/NoteLinksProcessorTest.java
@@ -1,4 +1,4 @@
-package it.niedermann.owncloud.notes.shared.util;
+package it.niedermann.owncloud.notes.shared.util.text;
 
 import junit.framework.TestCase;
 
@@ -8,18 +8,21 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static it.niedermann.owncloud.notes.shared.util.NoteLinksUtils.RELATIVE_LINK_WORKAROUND_PREFIX;
+import static it.niedermann.owncloud.notes.shared.util.text.NoteLinksProcessor.RELATIVE_LINK_WORKAROUND_PREFIX;
 
-
-public class NoteLinksUtilsTest extends TestCase {
+public class NoteLinksProcessorTest extends TestCase {
 
     public void testEmptyString() {
+        TextProcessor sut = new NoteLinksProcessor(Collections.emptySet());
+
         String markdown = "";
-        String result = NoteLinksUtils.replaceNoteLinksWithDummyUrls(markdown, Collections.emptySet());
+        String result = sut.process(markdown);
         Assert.assertEquals("", result);
     }
 
     public void testDoNotChangeOtherMarkdownElements() {
+        TextProcessor sut = new NoteLinksProcessor(Collections.emptySet());
+
         //language=md
         String markdown = "\n" +
                 "# heading  \n" +
@@ -37,14 +40,16 @@ public class NoteLinksUtilsTest extends TestCase {
                 "**Everything** else could be in here.\n" +
                 "\n";
 
-        Assert.assertEquals(markdown, NoteLinksUtils.replaceNoteLinksWithDummyUrls(markdown, Collections.emptySet()));
+        Assert.assertEquals(markdown, sut.process(markdown));
     }
 
     @SuppressWarnings("MarkdownUnresolvedFileReference")
     public void testDoNotReplaceNormalLinks() {
+        TextProcessor sut = new NoteLinksProcessor(Collections.singleton("123456"));
+
         //language=md
         String markdown = "[normal link](https://example.com) and another [note link](123456)";
-        String result = NoteLinksUtils.replaceNoteLinksWithDummyUrls(markdown, Collections.singleton("123456"));
+        String result = sut.process(markdown);
         Assert.assertEquals(String.format("[normal link](https://example.com) and another [note link](%s123456)", RELATIVE_LINK_WORKAROUND_PREFIX), result);
     }
 
@@ -52,9 +57,12 @@ public class NoteLinksUtilsTest extends TestCase {
         Set<String> remoteIdsOfExistingNotes = new HashSet<>();
         remoteIdsOfExistingNotes.add("123456");
         remoteIdsOfExistingNotes.add("321456");
+
+        TextProcessor sut = new NoteLinksProcessor(remoteIdsOfExistingNotes);
+
         String markdown = "[link to real note](123456) and another [link to non-existing note](654321) and one more [another link to real note](321456)";
 
-        String result = NoteLinksUtils.replaceNoteLinksWithDummyUrls(markdown, remoteIdsOfExistingNotes);
+        String result = sut.process(markdown);
 
         String expected = String.format("[link to real note](%s123456) and another [link to non-existing note](654321) and one more [another link to real note](%s321456)", RELATIVE_LINK_WORKAROUND_PREFIX, RELATIVE_LINK_WORKAROUND_PREFIX);
         Assert.assertEquals(
