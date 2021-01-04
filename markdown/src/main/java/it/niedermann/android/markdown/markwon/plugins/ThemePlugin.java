e5daa14a0f57f97ac0dfdfa17e8fa9a2d4afc86e
Stefan Niedermann <info@niedermann.it>
2021-01-04 21:41:27 +0100

    Workaround selection background color of fenced code blocks

--- markdown/src/main/java/it/niedermann/android/markdown/markwon/plugins/ThemePlugin.java
+++ markdown/src/main/java/it/niedermann/android/markdown/markwon/plugins/ThemePlugin.java
@@ -3,6 +3,7 @@ package it.niedermann.android.markdown.markwon.plugins;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import io.noties.markwon.AbstractMarkwonPlugin;
 import io.noties.markwon.MarkwonPlugin;
@@ -11,7 +12,8 @@ import it.niedermann.android.markdown.R;
 
 public class ThemePlugin extends AbstractMarkwonPlugin {
 
-    @NonNull Context context;
+    @NonNull
+    Context context;
 
     private ThemePlugin(@NonNull Context context) {
         this.context = context;
@@ -25,8 +27,9 @@ public class ThemePlugin extends AbstractMarkwonPlugin {
     public void configureTheme(@NonNull MarkwonTheme.Builder builder) {
         super.configureTheme(builder);
         builder
-                .bulletWidth(context.getResources().getDimensionPixelSize(R.dimen.bullet_point_width))
                 .headingBreakHeight(0)
-                .headingTextSizeMultipliers(new float[]{1.45f, 1.35f, 1.25f, 1.15f, 1.1f, 1.05f});
+                .codeBlockBackgroundColor(ContextCompat.getColor(context, R.color.bg_code))
+                .headingTextSizeMultipliers(new float[]{1.45f, 1.35f, 1.25f, 1.15f, 1.1f, 1.05f})
+                .bulletWidth(context.getResources().getDimensionPixelSize(R.dimen.bullet_point_width));
     }
 }
