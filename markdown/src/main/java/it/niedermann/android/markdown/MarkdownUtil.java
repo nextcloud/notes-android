4d61520d48515962d8b7c418fc60580947eeddda
Stefan Niedermann <info@niedermann.it>
2021-03-13 11:37:09 +0100

    Use new emoji replacement

--- markdown/src/main/java/it/niedermann/android/markdown/MarkdownUtil.java
+++ markdown/src/main/java/it/niedermann/android/markdown/MarkdownUtil.java
@@ -62,13 +62,7 @@ public class MarkdownUtil {
      * Therefore we currently use {@link HtmlCompat} to filter supported spans from the output of {@link HtmlRenderer} as an intermediate step.
      */
     public static CharSequence renderForRemoteView(@NonNull Context context, @NonNull String content) {
-        String html = renderer.render(parser.parse(replaceCheckboxesWithEmojis(content)));
-        // Emojis are only available on Marshmallow
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            html = html.replace("[ ]", "☐");
-            html = html.replace("[x]", "☑️️");
-        }
-        return HtmlCompat.fromHtml(html, 0);
+        return HtmlCompat.fromHtml(renderer.render(parser.parse(replaceCheckboxesWithEmojis(content))), 0);
     }
 
     @NonNull
