3ddfde9a5418ba23aef752162c73d423a5e207a3
Stefan Niedermann <info@niedermann.it>
2021-05-11 21:15:56 +0200

    Sanitize API versions while migrating the database

--- app/src/main/java/it/niedermann/owncloud/notes/persistence/migration/Migration_22_23.java
+++ app/src/main/java/it/niedermann/owncloud/notes/persistence/migration/Migration_22_23.java
@@ -1,15 +1,32 @@
 package it.niedermann.owncloud.notes.persistence.migration;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.OnConflictStrategy;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import it.niedermann.owncloud.notes.persistence.entity.Account;
+import it.niedermann.owncloud.notes.shared.model.ApiVersion;
 
 /**
  * Add <code>displayName</code> property to {@link Account}.
  * <p>
  * See: <a href="https://github.com/stefan-niedermann/nextcloud-notes/issues/1079">#1079 Show DisplayName instead of uid attribute for LDAP users</a>
+ * <p>
+ * Sanitizes the stored API versions in the database.
  */
 public class Migration_22_23 extends Migration {
 
@@ -19,6 +36,64 @@ public class Migration_22_23 extends Migration {
 
     @Override
     public void migrate(@NonNull SupportSQLiteDatabase db) {
+        addDisplayNameToAccounts(db);
+        sanitizeAccounts(db);
+    }
+
+    private static void addDisplayNameToAccounts(@NonNull SupportSQLiteDatabase db) {
         db.execSQL("ALTER TABLE Account ADD COLUMN displayName TEXT");
+    }
+
+    private static void sanitizeAccounts(@NonNull SupportSQLiteDatabase db) {
+        final Cursor cursor = db.query("SELECT ID, APIVERSION FROM ACCOUNTS", null);
+        final ContentValues values = new ContentValues(1);
+
+        final int COLUMN_POSITION_ID = cursor.getColumnIndex("ID");
+        final int COLUMN_POSITION_APIVERSION = cursor.getColumnIndex("APIVERSION");
+
+        while (cursor.moveToNext()) {
+            values.put("APIVERSION", sanitizeApiVersion(cursor.getString(COLUMN_POSITION_APIVERSION)));
+            db.update("ACCOUNT", OnConflictStrategy.REPLACE, values, "ID = ?", new String[]{String.valueOf(cursor.getLong(COLUMN_POSITION_ID))});
+        }
+        cursor.close();
+    }
+
+    @Nullable
+    public static String sanitizeApiVersion(@Nullable String raw) {
+        if (TextUtils.isEmpty(raw)) {
+            return null;
+        }
+
+        JSONArray a;
+        try {
+            a = new JSONArray(raw);
+        } catch (JSONException e) {
+            try {
+                a = new JSONArray("[" + raw + "]");
+            } catch (JSONException e1) {
+                return null;
+            }
+        }
+
+        final Collection<ApiVersion> result = new ArrayList<>();
+        for (int i = 0; i < a.length(); i++) {
+            try {
+                final ApiVersion version = ApiVersion.of(a.getString(i));
+                if (version.getMajor() != 0 || version.getMinor() != 0) {
+                    result.add(version);
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        if (result.isEmpty()) {
+            return null;
+        }
+        return "[" +
+                result
+                        .stream()
+                        .filter(Objects::nonNull)
+                        .map(v -> v.getMajor() + "." + v.getMinor())
+                        .collect(Collectors.joining(","))
+                + "]";
     }
 }
