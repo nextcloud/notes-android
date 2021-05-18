9d626faa047915a02c2fb5a403bccd42b8511347
alpaca0072 <AlpAcA0072@gmail.com>
2021-05-18 20:57:12 +0800

    [Store category sorting for recent, favorites and uncategorized per account](https://github.com/stefan-niedermann/nextcloud-notes/issues/1169)

--- app/src/main/java/it/niedermann/owncloud/notes/persistence/migration/Migration_23_24.java
+++ app/src/main/java/it/niedermann/owncloud/notes/persistence/migration/Migration_23_24.java
@@ -33,20 +33,23 @@ public class Migration_23_24 extends Migration {
 
         while (cursor.moveToNext()) {
             long accountId = cursor.getLong(COLUMN_POSITION_ID);
-            resetSharedPreferences(sharedPreferences, editor, R.string.action_uncategorized, accountId);
-            resetSharedPreferences(sharedPreferences, editor, R.string.label_favorites, accountId);
-            resetSharedPreferences(sharedPreferences, editor, R.string.label_all_notes, accountId);
+            resetSharedPreferences(sharedPreferences, editor, "Uncategorized", accountId);
+            resetSharedPreferences(sharedPreferences, editor, "Favorites", accountId);
+            resetSharedPreferences(sharedPreferences, editor, "All notes", accountId);
         }
+
+        editor.remove("Sorting method" + ' ' + "Uncategorized");
+        editor.remove("Sorting method" + ' ' + "Favorites");
+        editor.remove("Sorting method" + ' ' + "All notes");
         editor.apply();
         cursor.close();
     }
 
 
-    private void resetSharedPreferences(SharedPreferences sharedPreferences, SharedPreferences.Editor editor, int label, long accountId) {
-        final String key = context.getString(R.string.action_sorting_method) + ' ' + context.getString(label);
+    private void resetSharedPreferences(SharedPreferences sharedPreferences, SharedPreferences.Editor editor, String label, long accountId) {
+        final String key = "Sorting method" + ' ' + label;
         if (sharedPreferences.contains(key)) {
             int sortingMethod = sharedPreferences.getInt(key, 0);
-            editor.remove(key);
             editor.putInt(key + accountId, sortingMethod);
         }
     }
