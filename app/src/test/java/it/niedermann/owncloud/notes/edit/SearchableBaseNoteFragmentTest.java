48caba2b8aa5bfb94416415d845abaffa16ee277
Stefan Niedermann <info@niedermann.it>
2021-05-18 09:46:02 +0200

    Adjust test style

--- app/src/test/java/it/niedermann/owncloud/notes/edit/SearchableBaseNoteFragmentTest.java
+++ app/src/test/java/it/niedermann/owncloud/notes/edit/SearchableBaseNoteFragmentTest.java
@@ -3,61 +3,49 @@ package it.niedermann.owncloud.notes.edit;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class SearchableBaseNoteFragmentTest {
 
     @SuppressWarnings("ConstantConditions")
     @Test
-    public void testCountOccurrencesFixed() {
-        try {
-            Method method = SearchableBaseNoteFragment.class.getDeclaredMethod("countOccurrences", String.class, String.class);
-            method.setAccessible(true);
+    public void testCountOccurrencesFixed() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        final Method method = SearchableBaseNoteFragment.class.getDeclaredMethod("countOccurrences", String.class, String.class);
+        method.setAccessible(true);
 
-            for (int count = 0; count <= 15; ++count) {
-                StringBuilder sb = new StringBuilder("Mike Chester Wang");
-                for (int i = 0; i < count; ++i) {
-                    sb.append(sb);
-                }
-
-                long startTime = System.currentTimeMillis();
-                int num = (int) method.invoke(null, sb.toString(), "Chester");
-                long endTime = System.currentTimeMillis();
-                System.out.println("Fixed Version");
-                System.out.println("Total Time: " + (endTime - startTime) + " ms");
-                System.out.println("Total Times: " + num);
-                System.out.println("String Size: " + (sb.length() / 1024.0) + " K");
-                Assert.assertEquals((int) Math.pow(2, count), num);
-                System.out.println();
+        for (int count = 0; count <= 15; ++count) {
+            final StringBuilder sb = new StringBuilder("Mike Chester Wang");
+            for (int i = 0; i < count; ++i) {
+                sb.append(sb);
             }
 
-        } catch (Exception e) {
-            Assert.fail(Arrays.toString(e.getStackTrace()));
-            System.out.println("Test Count Occurrences Fixed" + Arrays.toString(e.getStackTrace()));
+            final long startTime = System.currentTimeMillis();
+            final int num = (int) method.invoke(null, sb.toString(), "Chester");
+            final long endTime = System.currentTimeMillis();
+            System.out.println("Fixed Version");
+            System.out.println("Total Time: " + (endTime - startTime) + " ms");
+            System.out.println("Total Times: " + num);
+            System.out.println("String Size: " + (sb.length() / 1024.0) + " K");
+            Assert.assertEquals((int) Math.pow(2, count), num);
+            System.out.println();
         }
     }
 
     @SuppressWarnings("ConstantConditions")
     @Test
-    public void testNullOrEmptyInput() {
-        try {
-            Method method = SearchableBaseNoteFragment.class.getDeclaredMethod("countOccurrences", String.class, String.class);
-            method.setAccessible(true);
+    public void testNullOrEmptyInput() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        final Method method = SearchableBaseNoteFragment.class.getDeclaredMethod("countOccurrences", String.class, String.class);
+        method.setAccessible(true);
 
-            int num;
-            num = (int) method.invoke(null, null, "Hi");
-            Assert.assertEquals(0, num);
-            num = (int) method.invoke(null, "Hi my name is Mike Chester Wang", null);
-            Assert.assertEquals(0, num);
-            num = (int) method.invoke(null, "", "Hi");
-            Assert.assertEquals(0, num);
-            num = (int) method.invoke(null, "Hi my name is Mike Chester Wang", "");
-            Assert.assertEquals(0, num);
-
-        } catch (Exception e) {
-            Assert.fail(Arrays.toString(e.getStackTrace()));
-            System.out.println("Test Null Or Empty Input" + Arrays.toString(e.getStackTrace()));
-        }
+        int num;
+        num = (int) method.invoke(null, null, "Hi");
+        Assert.assertEquals(0, num);
+        num = (int) method.invoke(null, "Hi my name is Mike Chester Wang", null);
+        Assert.assertEquals(0, num);
+        num = (int) method.invoke(null, "", "Hi");
+        Assert.assertEquals(0, num);
+        num = (int) method.invoke(null, "Hi my name is Mike Chester Wang", "");
+        Assert.assertEquals(0, num);
     }
 }
