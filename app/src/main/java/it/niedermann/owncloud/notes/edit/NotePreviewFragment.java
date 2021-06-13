fc598b84c94765e603c364abdfa568454443aa2d
Stefan Niedermann <info@niedermann.it>
2021-06-13 11:51:48 +0200

    Remove unused import
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/owncloud/notes/edit/NotePreviewFragment.java
+++ app/src/main/java/it/niedermann/owncloud/notes/edit/NotePreviewFragment.java
@@ -2,7 +2,6 @@ package it.niedermann.owncloud.notes.edit;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Layout;
