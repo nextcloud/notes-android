1f1a0428f5cce6e34bae787682bcf6980826df4c
Stefan Niedermann <info@niedermann.it>
2021-07-23 15:07:24 +0200

    Add convenience methods for TextWatcherIntegrationTest
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- markdown/src/test/java/it/niedermann/android/markdown/markwon/textwatcher/TextWatcherIntegrationTest.java
+++ markdown/src/test/java/it/niedermann/android/markdown/markwon/textwatcher/TextWatcherIntegrationTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import it.niedermann.android.markdown.markwon.MarkwonMarkdownEditor;
+import it.niedermann.android.markdown.model.EListType;
 
 import static android.view.KeyEvent.KEYCODE_1;
 import static android.view.KeyEvent.KEYCODE_A;
@@ -25,18 +26,15 @@ import static android.view.KeyEvent.KEYCODE_CTRL_LEFT;
 import static android.view.KeyEvent.KEYCODE_F;
 import static android.view.KeyEvent.KEYCODE_LEFT_BRACKET;
 import static android.view.KeyEvent.KEYCODE_MINUS;
-import static android.view.KeyEvent.KEYCODE_NUMPAD_DOT;
 import static android.view.KeyEvent.KEYCODE_O;
 import static android.view.KeyEvent.KEYCODE_PERIOD;
 import static android.view.KeyEvent.KEYCODE_Q;
 import static android.view.KeyEvent.KEYCODE_R;
 import static android.view.KeyEvent.KEYCODE_RIGHT_BRACKET;
-import static android.view.KeyEvent.KEYCODE_S;
 import static android.view.KeyEvent.KEYCODE_SPACE;
 import static android.view.KeyEvent.KEYCODE_STAR;
 import static android.view.KeyEvent.KEYCODE_U;
 import static android.view.KeyEvent.KEYCODE_X;
-import static android.view.KeyEvent.KEYCODE_Z;
 
 @RunWith(RobolectricTestRunner.class)
 public class TextWatcherIntegrationTest extends TestCase {
@@ -56,91 +54,101 @@ public class TextWatcherIntegrationTest extends TestCase {
         this.editText.setText("");
         assertEquals("");
 
-        sendKey(KEYCODE_F, KEYCODE_O, KEYCODE_O);
+        sendKeys(KEYCODE_F, KEYCODE_O, KEYCODE_O);
         assertEquals("foo");
 
-        pressEnter(3);
+        pressEnter();
         assertEquals("foo\n");
 
         sendCheckboxKeys();
         assertEquals("foo\n- [ ] ");
 
-        pressEnter(10);
+        pressEnter();
         assertEquals("foo\n\n");
 
         sendCheckboxKeys();
-        sendKey(KEYCODE_B, KEYCODE_A, KEYCODE_R);
+        sendKeys(KEYCODE_B, KEYCODE_A, KEYCODE_R);
         assertEquals("foo\n\n- [ ] bar");
 
-        pressEnter(14);
+        pressEnter();
         assertEquals("foo\n\n- [ ] bar\n- [ ] ");
 
-        pressEnter(21);
+        pressEnter();
         assertEquals("foo\n\n- [ ] bar\n\n");
 
-        pressBackspace(16);
+        pressBackspace();
         assertEquals("foo\n\n- [ ] bar\n");
 
-        sendKey(KEYCODE_SPACE, KEYCODE_SPACE);
+        sendKeys(KEYCODE_SPACE, KEYCODE_SPACE);
         sendCheckboxKeys();
-        sendKey(KEYCODE_B, KEYCODE_A, KEYCODE_R);
+        sendKeys(KEYCODE_B, KEYCODE_A, KEYCODE_R);
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar");
 
-        pressEnter(26);
+        pressEnter();
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar\n  - [ ] ");
 
-        pressBackspace(35);
+        pressBackspace();
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar\n- [ ] ");
 
-        pressBackspace(33);
-        pressBackspace(32);
-        pressBackspace(31);
-        pressBackspace(30);
-        pressBackspace(29);
-        pressBackspace(28);
+        pressBackspace(6, 33);
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar\n");
 
-        sendKey(KEYCODE_SPACE, KEYCODE_SPACE, KEYCODE_1, KEYCODE_PERIOD, KEYCODE_SPACE, KEYCODE_Q, KEYCODE_U, KEYCODE_X);
+        sendKeys(KEYCODE_SPACE, KEYCODE_SPACE, KEYCODE_1, KEYCODE_PERIOD, KEYCODE_SPACE, KEYCODE_Q, KEYCODE_U, KEYCODE_X);
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar\n  1. qux");
 
-        pressEnter(35);
+        pressEnter();
         assertEquals("foo\n\n- [ ] bar\n  - [ ] bar\n  1. qux\n  2. ");
 
-        editText.setSelection(14);
         pressEnter(14);
-        pressBackspace(21);
-        pressBackspace(20);
-        pressBackspace(19);
-        pressBackspace(18);
-        pressBackspace(17);
-        pressBackspace(16);
+        pressBackspace(6, 21);
         assertEquals("foo\n\n- [ ] bar\n\n  - [ ] bar\n  1. qux\n  2. ");
 
-        sendKey(KEYCODE_SPACE, KEYCODE_SPACE, KEYCODE_STAR);
-        pressBackspace(18);
-        sendKey(KEYCODE_STAR, KEYCODE_SPACE, KEYCODE_A, KEYCODE_B, KEYCODE_C);
+        sendKeys(KEYCODE_SPACE, KEYCODE_SPACE, KEYCODE_STAR);
+        pressBackspace();
+        sendKeys(KEYCODE_STAR, KEYCODE_SPACE, KEYCODE_A, KEYCODE_B, KEYCODE_C);
         assertEquals("foo\n\n- [ ] bar\n  * abc\n  - [ ] bar\n  1. qux\n  2. ");
     }
 
+
+    // Convenience methods
+
     private void sendCheckboxKeys() {
-        sendKey(KEYCODE_MINUS, KEYCODE_SPACE, KEYCODE_CTRL_LEFT, KEYCODE_LEFT_BRACKET, KEYCODE_SPACE, KEYCODE_RIGHT_BRACKET, KEYCODE_SPACE);
+        sendKeys(KEYCODE_MINUS, KEYCODE_SPACE, KEYCODE_CTRL_LEFT, KEYCODE_LEFT_BRACKET, KEYCODE_SPACE, KEYCODE_RIGHT_BRACKET, KEYCODE_SPACE);
     }
 
     private void assertEquals(@NonNull CharSequence expected) {
         assertEquals(expected.toString(), editText.getText().toString());
     }
 
-    private void pressBackspace(int atPosition) {
+    private void pressEnter(@SuppressWarnings("SameParameterValue") int atPosition) {
         this.editText.setSelection(atPosition);
-        this.editText.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+        pressEnter();
     }
 
-    private void pressEnter(int atPosition) {
-        this.editText.setSelection(atPosition);
+    private void pressEnter() {
         this.editText.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
     }
 
-    private void sendKey(int... keyCodes) {
+    private void pressBackspace(@SuppressWarnings("SameParameterValue") int times, int startPosition) {
+        if (times > startPosition) {
+            throw new IllegalArgumentException("startPosition must be bigger or equal to times");
+        }
+        while (times > 0) {
+            times--;
+            pressBackspace(startPosition--);
+        }
+    }
+
+    private void pressBackspace(int atPosition) {
+        this.editText.setSelection(atPosition);
+        pressBackspace();
+    }
+
+    private void pressBackspace() {
+        this.editText.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+    }
+
+    private void sendKeys(int... keyCodes) {
         int release = -1;
         for (int k : keyCodes) {
             if (release != -1) {
