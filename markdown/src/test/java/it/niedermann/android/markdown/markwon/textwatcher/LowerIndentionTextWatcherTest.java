f465c3f0386720639e286a99490064b5ba9c9cfd
Stefan Niedermann <info@niedermann.it>
2021-07-23 11:11:05 +0200

    Add TextWatcherIntegrationTest
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- markdown/src/test/java/it/niedermann/android/markdown/markwon/textwatcher/LowerIndentionTextWatcherTest.java
+++ markdown/src/test/java/it/niedermann/android/markdown/markwon/textwatcher/LowerIndentionTextWatcherTest.java
@@ -1,34 +1,11 @@
 package it.niedermann.android.markdown.markwon.textwatcher;
 
-import android.view.KeyEvent;
-import android.widget.EditText;
-
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.test.core.app.ApplicationProvider;
-
-import junit.framework.TestCase;
-
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import it.niedermann.android.markdown.markwon.MarkwonMarkdownEditor;
-import it.niedermann.android.markdown.model.EListType;
-
 @RunWith(RobolectricTestRunner.class)
-public class LowerIndentionTextWatcherTest extends TestCase {
-
-    @Rule
-    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
-
-    private EditText editText;
-
-    @Before
-    public void reset() {
-        this.editText = new MarkwonMarkdownEditor(ApplicationProvider.getApplicationContext());
-    }
+public class LowerIndentionTextWatcherTest extends AbstractTextWatcherTest {
 
     @Test
     public void shouldLowerIndentionByTwoWhenPressingBackspaceOnAnIndentedList() {
@@ -106,16 +83,5 @@ public class LowerIndentionTextWatcherTest extends TestCase {
         this.editText.setText("- [ ] Foo");
         pressBackspace(9);
         assertText("- [ ] Fo", 8);
-    }
-
-    private void pressBackspace(int atPosition) {
-        this.editText.setSelection(atPosition);
-        this.editText.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL, 0));
-    }
-
-    private void assertText(String expected, int cursorPosition) {
-        assertEquals(expected, this.editText.getText().toString());
-        assertEquals(cursorPosition, this.editText.getSelectionStart());
-        assertEquals(cursorPosition, this.editText.getSelectionEnd());
     }
 }
