612e40053e3b16531c24b6f3630bd07bcaf90353
alperozturk <alper_ozturk@proton.me>
2025-01-27 13:58:46 +0100

    add viewholders
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/it/niedermann/owncloud/notes/share/adapter/holder/InternalShareViewHolder.java
@@ -0,0 +1,55 @@
+package it.niedermann.owncloud.notes.share.adapter.holder;
+
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.view.View;
+
+import com.owncloud.android.lib.resources.shares.OCShare;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import it.niedermann.owncloud.notes.R;
+import it.niedermann.owncloud.notes.databinding.ItemInternalShareLinkBinding;
+import it.niedermann.owncloud.notes.share.listener.ShareeListAdapterListener;
+
+public class InternalShareViewHolder extends RecyclerView.ViewHolder {
+    private ItemInternalShareLinkBinding binding;
+    private Context context;
+
+    public InternalShareViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public InternalShareViewHolder(ItemInternalShareLinkBinding binding, Context context) {
+        this(binding.getRoot());
+        this.binding = binding;
+        this.context = context;
+    }
+
+    public void bind(OCShare share, ShareeListAdapterListener listener) {
+        binding.copyInternalLinkIcon
+                .getBackground()
+                .setColorFilter(ResourcesCompat.getColor(context.getResources(),
+                                R.color.widget_foreground,
+                                null),
+                        PorterDuff.Mode.SRC_IN);
+        binding.copyInternalLinkIcon
+                .getDrawable()
+                .mutate()
+                .setColorFilter(ResourcesCompat.getColor(context.getResources(),
+                                R.color.fg_contrast,
+                                null),
+                        PorterDuff.Mode.SRC_IN);
+
+        if (share.isFolder()) {
+            binding.shareInternalLinkText.setText(context.getString(R.string.share_internal_link_to_folder_text));
+        } else {
+            binding.shareInternalLinkText.setText(context.getString(R.string.share_internal_link_to_file_text));
+        }
+
+        binding.copyInternalContainer.setOnClickListener(l -> listener.copyInternalLink());
+    }
+}
