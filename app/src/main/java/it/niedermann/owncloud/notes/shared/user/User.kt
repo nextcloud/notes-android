9f825a8d474bfd6428d83698f0286850e4e5be85
alperozturk <alper_ozturk@proton.me>
2025-01-27 12:05:17 +0100

    add some logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/it/niedermann/owncloud/notes/shared/user/User.kt
@@ -0,0 +1,56 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2020 Chris Narkiewicz <hello@ezaquarii.com>
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
+ */
+package it.niedermann.owncloud.notes.shared.user
+
+import android.accounts.Account
+import android.os.Parcelable
+import com.owncloud.android.lib.common.OwnCloudAccount
+import it.niedermann.owncloud.notes.shared.server.Server
+
+interface User : Parcelable, com.nextcloud.common.User {
+    override val accountName: String
+    val server: Server
+
+    /**
+     * This is temporary helper method created to facilitate incremental refactoring.
+     * Code using legacy platform Account can be partially converted to instantiate User
+     * object and use account instance when required.
+     *
+     * This method calls will allow tracing code awaiting further refactoring.
+     *
+     * @return Account instance that is associated with this User object.
+     */
+    @Deprecated("Temporary workaround")
+    override fun toPlatformAccount(): Account
+
+    /**
+     * This is temporary helper method created to facilitate incremental refactoring.
+     * Code using legacy ownCloud account can be partially converted to instantiate User
+     * object and use account instance when required.
+     *
+     * This method calls will allow tracing code awaiting further refactoring.
+     *
+     * @return OwnCloudAccount instance that is associated with this User object.
+     */
+    @Deprecated("Temporary workaround")
+    fun toOwnCloudAccount(): OwnCloudAccount
+
+    /**
+     * Compare account names, case insensitive.
+     *
+     * @return true if account names are same, false otherwise
+     */
+    fun nameEquals(user: User?): Boolean
+
+    /**
+     * Compare account names, case insensitive.
+     *
+     * @return true if account names are same, false otherwise
+     */
+    fun nameEquals(accountName: CharSequence?): Boolean
+}
