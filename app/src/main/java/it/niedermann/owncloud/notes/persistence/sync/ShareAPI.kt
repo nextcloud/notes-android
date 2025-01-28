70412aafc33b5adeaf0c74274d3054d28d14db09
alperozturk <alper_ozturk@proton.me>
2025-01-28 16:33:36 +0100

    use share api
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/it/niedermann/owncloud/notes/persistence/sync/ShareAPI.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/persistence/sync/ShareAPI.kt
@@ -3,14 +3,36 @@ package it.niedermann.owncloud.notes.persistence.sync
 import com.nextcloud.android.sso.api.EmptyResponse
 import com.owncloud.android.lib.resources.shares.OCShare
 import com.owncloud.android.lib.resources.shares.ShareType
+import it.niedermann.owncloud.notes.share.model.ShareesData
 import it.niedermann.owncloud.notes.shared.model.OcsResponse
 import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ShareAPI {
+    @GET("sharees")
+    fun getSharees2(
+        @Query("format") format: String = "json",
+        @Query("itemType") itemType: String = "note",
+        @Query("search") search: String,
+        @Query("page") page: Int,
+        @Query("perPage") perPage: Int,
+        @Query("lookup") lookup: Boolean = true,
+    ): Call<Any>
+
+    @GET("sharees")
+    fun getSharees(
+        @Query("format") format: String = "json",
+        @Query("itemType") itemType: String = "note",
+        @Query("search") search: String,
+        @Query("page") page: Int,
+        @Query("perPage") perPage: Int,
+        @Query("lookup") lookup: Boolean = true,
+    ): Call<OcsResponse<ShareesData>>
+
     @GET("shares")
     fun getShares(remoteId: Long): Call<OcsResponse<List<OCShare>>>
 
