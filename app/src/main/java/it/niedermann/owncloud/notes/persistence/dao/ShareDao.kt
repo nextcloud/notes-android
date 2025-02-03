534257e798fedfc4ba710e20d590044f689705f6
alperozturk <alper_ozturk@proton.me>
2025-02-03 15:21:40 +0100

    fix build
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/it/niedermann/owncloud/notes/persistence/dao/ShareDao.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/persistence/dao/ShareDao.kt
@@ -2,15 +2,15 @@ package it.niedermann.owncloud.notes.persistence.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import it.niedermann.owncloud.notes.persistence.entity.ShareEntity
 
 @Dao
 interface ShareDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun addShareEntities(entities: List<ShareEntity>)
 
-    @Query("SELECT * FROM share_table WHERE path = :path")
-    fun getShareEntities(path: String): List<ShareEntity>
+    @Insert
+    fun addShareEntity(entity: ShareEntity)
+
+    @Query("SELECT * FROM share_table WHERE noteRemoteId = :noteRemoteId AND userName = :userName")
+    fun getShareEntities(noteRemoteId: Long, userName: String): List<ShareEntity>
 }
