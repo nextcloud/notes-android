0db720c3a16a0ecd38766650543e7256ce3541d6
alperozturk <alper_ozturk@proton.me>
2025-07-07 15:21:00 +0200

    get note in background thread
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/it/niedermann/owncloud/notes/branding/BrandedFragment.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/branding/BrandedFragment.kt
@@ -4,60 +4,85 @@
  * SPDX-FileCopyrightText: 2020-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-package it.niedermann.owncloud.notes.branding;
+package it.niedermann.owncloud.notes.branding
 
-import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.annotation.ColorInt
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.nextcloud.android.common.ui.util.extensions.adjustUIForAPILevel35
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-import androidx.annotation.ColorInt;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.nextcloud.android.common.ui.util.extensions.AppCompatActivityExtensionsKt;
-
-public abstract class BrandedFragment extends Fragment implements Branded {
-
+abstract class BrandedFragment : Fragment(), Branded {
+    @JvmField
     @ColorInt
-    protected int colorAccent;
-    @ColorInt
-    protected int colorPrimary;
+    protected var colorAccent: Int = 0
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        if (getActivity() instanceof AppCompatActivity appCompatActivity) {
-            AppCompatActivityExtensionsKt.adjustUIForAPILevel35(appCompatActivity);
+    @JvmField
+    @ColorInt
+    protected var colorPrimary: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (activity is AppCompatActivity) {
+            val appCompatActivity = activity as AppCompatActivity
+            appCompatActivity.adjustUIForAPILevel35()
         }
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    override fun onStart() {
+        super.onStart()
 
-        final var context = requireContext();
-        final var typedValue = new TypedValue();
-        context.getTheme().resolveAttribute(com.google.android.material.R.attr.colorAccent, typedValue, true);
-        colorAccent = typedValue.data;
-        context.getTheme().resolveAttribute(com.google.android.material.R.attr.colorPrimary, typedValue, true);
-        colorPrimary = typedValue.data;
+        val context = requireContext()
+        val typedValue = TypedValue()
 
-        @ColorInt final int color = BrandingUtil.readBrandMainColor(context);
-        applyBrand(color);
+        context.theme.resolveAttribute(
+            com.google.android.material.R.attr.colorAccent,
+            typedValue,
+            true
+        )
+        colorAccent = typedValue.data
+
+        context.theme.resolveAttribute(
+            com.google.android.material.R.attr.colorPrimary,
+            typedValue,
+            true
+        )
+        colorPrimary = typedValue.data
+
+        @ColorInt
+        val color = BrandingUtil.readBrandMainColor(context)
+        applyBrand(color)
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        final var utils = BrandingUtil.of(colorAccent, requireContext());
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        val utils = BrandingUtil.of(colorAccent, requireContext())
 
-        for (int i = 0; i < menu.size(); i++) {
-            if (menu.getItem(i).getIcon() != null) {
-                utils.platform.colorToolbarMenuIcon(requireContext(), menu.getItem(i));
+        menu.forEach { menu ->
+            menu.icon?.let { icon ->
+                utils.platform.colorToolbarMenuIcon(requireContext(), menu)
             }
+        }
+    }
+
+    fun lifecycleScopeIOJob(block: () -> Unit) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            block()
+        }
+    }
+
+    fun onMainThread(block: () -> Unit) {
+        activity?.runOnUiThread {
+            block()
         }
     }
 }
