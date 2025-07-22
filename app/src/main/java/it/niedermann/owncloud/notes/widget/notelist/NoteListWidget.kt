a78ee276a6e417722abc61848667f54e6d272699
alperozturk <alper_ozturk@proton.me>
2025-07-22 16:48:39 +0200

    fix
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/it/niedermann/owncloud/notes/widget/notelist/NoteListWidget.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/widget/notelist/NoteListWidget.kt
@@ -1,118 +1,135 @@
 /*
  * Nextcloud Notes - Android Client
  *
- * SPDX-FileCopyrightText: 2017-2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2017-2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-package it.niedermann.owncloud.notes.widget.notelist;
+package it.niedermann.owncloud.notes.widget.notelist
 
-import static it.niedermann.owncloud.notes.shared.util.WidgetUtil.pendingIntentFlagCompat;
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.widget.RemoteViews
+import com.owncloud.android.lib.common.utils.Log_OC
+import it.niedermann.owncloud.notes.R
+import it.niedermann.owncloud.notes.edit.EditNoteActivity
+import it.niedermann.owncloud.notes.persistence.NotesRepository
+import it.niedermann.owncloud.notes.shared.util.WidgetUtil
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import androidx.core.net.toUri
 
-import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
-import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
-import android.widget.RemoteViews;
+class NoteListWidget : AppWidgetProvider() {
+    private val executor: ExecutorService = Executors.newCachedThreadPool()
 
-import java.util.NoSuchElementException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import it.niedermann.owncloud.notes.R;
-import it.niedermann.owncloud.notes.edit.EditNoteActivity;
-import it.niedermann.owncloud.notes.persistence.NotesRepository;
-
-public class NoteListWidget extends AppWidgetProvider {
-    private static final String TAG = NoteListWidget.class.getSimpleName();
-    private final ExecutorService executor = Executors.newCachedThreadPool();
-
-    static void updateAppWidget(Context context, AppWidgetManager awm, int[] appWidgetIds) {
-        final var repo = NotesRepository.getInstance(context);
-
-        RemoteViews views;
-
-        for (int appWidgetId : appWidgetIds) {
-            try {
-                final var data = repo.getNoteListWidgetData(appWidgetId);
-
-                final var serviceIntent = new Intent(context, NoteListWidgetService.class);
-                serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-                serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
-
-                Log.v(TAG, "-- data - " + data);
-
-                Intent editNoteIntent = new Intent(context, EditNoteActivity.class);
-                editNoteIntent.setPackage(context.getPackageName());
-
-                int pendingIntentFlags = pendingIntentFlagCompat(PendingIntent.FLAG_UPDATE_CURRENT | Intent.FILL_IN_COMPONENT);
-                PendingIntent editNotePendingIntent = PendingIntent.getActivity(context, 0, editNoteIntent, pendingIntentFlags);
-
-                views = new RemoteViews(context.getPackageName(), R.layout.widget_note_list);
-                views.setRemoteAdapter(R.id.note_list_widget_lv, serviceIntent);
-                views.setPendingIntentTemplate(R.id.note_list_widget_lv, editNotePendingIntent);
-                views.setEmptyView(R.id.note_list_widget_lv, R.id.widget_note_list_placeholder_tv);
-
-                awm.notifyAppWidgetViewDataChanged(appWidgetId, R.id.note_list_widget_lv);
-                awm.updateAppWidget(appWidgetId, views);
-            } catch (NoSuchElementException e) {
-                Log.i(TAG, "onUpdate has been triggered before the user finished configuring the widget");
-            }
-        }
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        updateAppWidget(context, appWidgetManager, appWidgetIds)
     }
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        updateAppWidget(context, appWidgetManager, appWidgetIds);
-    }
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        val awm = AppWidgetManager.getInstance(context)
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        final var awm = AppWidgetManager.getInstance(context);
-
-        if (intent.getAction() == null) {
-            Log.w(TAG, "Intent action is null");
-            return;
+        if (intent.action == null) {
+            Log.w(TAG, "Intent action is null")
+            return
         }
 
-        if (!intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-            Log.w(TAG, "Intent action is not ACTION_APPWIDGET_UPDATE");
-            return;
+        if (intent.action != AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
+            Log.w(TAG, "Intent action is not ACTION_APPWIDGET_UPDATE")
+            return
         }
 
         if (!intent.hasExtra(AppWidgetManager.EXTRA_APPWIDGET_ID)) {
-            Log.w(TAG,"Update widget via default appWidgetIds");
-            updateAppWidget(context, awm, awm.getAppWidgetIds(new ComponentName(context, NoteListWidget.class)));
+            Log.w(TAG, "Update widget via default appWidgetIds")
+            updateAppWidget(
+                context,
+                awm,
+                awm.getAppWidgetIds(ComponentName(context, NoteListWidget::class.java))
+            )
         }
 
-        if (intent.getExtras() == null) {
-            Log.w(TAG, "Intent doesn't have bundle");
-            return;
+        if (intent.extras == null) {
+            Log.w(TAG, "Intent doesn't have bundle")
+            return
         }
 
-        Log.w(TAG,"Update widget via given appWidgetIds");
-        updateAppWidget(context, awm, new int[]{intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)});
+        Log.w(TAG, "Update widget via given appWidgetIds")
+
+        val appWidgetIds = intArrayOf(intent.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, -1) ?: -1)
+
+        updateAppWidget(
+            context,
+            awm,
+            appWidgetIds
+        )
     }
 
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        super.onDeleted(context, appWidgetIds);
-        final var repo = NotesRepository.getInstance(context);
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        super.onDeleted(context, appWidgetIds)
+        val repo = NotesRepository.getInstance(context)
 
-        for (final int appWidgetId : appWidgetIds) {
-            executor.submit(() -> repo.removeNoteListWidget(appWidgetId));
+        for (appWidgetId in appWidgetIds) {
+            executor.submit(Runnable { repo.removeNoteListWidget(appWidgetId) })
         }
     }
 
-    /**
-     * Update note list widgets, if the note data was changed.
-     */
-    public static void updateNoteListWidgets(Context context) {
-        context.sendBroadcast(new Intent(context, NoteListWidget.class).setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE));
+    companion object {
+        private val TAG: String = NoteListWidget::class.java.getSimpleName()
+        fun updateAppWidget(context: Context, awm: AppWidgetManager, appWidgetIds: IntArray) {
+            val repo = NotesRepository.getInstance(context)
+            appWidgetIds.forEach { appWidgetId ->
+                repo.getNoteListWidgetData(appWidgetId)?.let { data ->
+                    val serviceIntent = Intent(context, NoteListWidgetService::class.java).apply {
+                        putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                        setData(toUri(Intent.URI_INTENT_SCHEME).toUri())
+                    }
+
+
+                    Log.v(TAG, "-- data - $data")
+
+                    val editNoteIntent = Intent(context, EditNoteActivity::class.java).apply {
+                        setPackage(context.packageName)
+                    }
+
+                    val pendingIntentFlags =
+                        WidgetUtil.pendingIntentFlagCompat(PendingIntent.FLAG_UPDATE_CURRENT or Intent.FILL_IN_COMPONENT)
+                    val editNotePendingIntent =
+                        PendingIntent.getActivity(context, 0, editNoteIntent, pendingIntentFlags)
+
+                    val views = RemoteViews(context.packageName, R.layout.widget_note_list).apply {
+                        setRemoteAdapter(R.id.note_list_widget_lv, serviceIntent)
+                        setPendingIntentTemplate(R.id.note_list_widget_lv, editNotePendingIntent)
+                        setEmptyView(
+                            R.id.note_list_widget_lv,
+                            R.id.widget_note_list_placeholder_tv
+                        )
+                    }
+
+                    awm.run {
+                        updateAppWidget(appWidgetId, views)
+                        notifyAppWidgetViewDataChanged(appWidgetId, R.id.note_list_widget_lv)
+                    }
+                }
+            }
+        }
+
+        @JvmStatic
+        fun updateNoteListWidgets(context: Context) {
+            val intent = Intent(context, NoteListWidget::class.java).apply {
+                setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+            }
+            context.sendBroadcast(intent)
+        }
     }
 }
