099e1e7834e7a5949465cebdbe2fb8333026af07
alperozturk96 <alper_ozturk@proton.me>
2026-02-10 11:21:30 +0100

    fix(share): permission
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/it/niedermann/owncloud/notes/share/model/QuickPermission.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/share/model/QuickPermission.kt
@@ -6,4 +6,4 @@
  */
 package it.niedermann.owncloud.notes.share.model
 
-data class QuickPermissionModel(val permissionName: String, val isSelected: Boolean)
+data class QuickPermission(val type: QuickPermissionType, var isSelected: Boolean)
