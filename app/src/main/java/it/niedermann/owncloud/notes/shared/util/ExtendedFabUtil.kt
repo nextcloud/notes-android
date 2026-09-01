040e6036b9e6e884a7fb88a92f1e5a27dc685af4
daniele-verducci <daniele.verducci@nextcloud.com>
2026-09-01 11:33:10 +0200

    2272: Avoid edit button to cover cursor when the text is scrolled due to the user deleting an entire line
    Signed-off-by: daniele-verducci <daniele.verducci@nextcloud.com>

--- app/src/main/java/it/niedermann/owncloud/notes/shared/util/ExtendedFabUtil.kt
+++ app/src/main/java/it/niedermann/owncloud/notes/shared/util/ExtendedFabUtil.kt
@@ -24,7 +24,7 @@ object ExtendedFabUtil {
             if (extendedFab.isExtended) {
                 extendedFab.hide()
             } else {
-                if (extendedFab.animation == null) {
+                if (extendedFab.animation == null && extendedFab.isShown) {
                     val animation =
                         AnimationUtils.loadAnimation(
                             extendedFab.context,
@@ -64,9 +64,10 @@ object ExtendedFabUtil {
         extendedFab: ExtendedFloatingActionButton,
         scrollY: Int,
         oldScrollY: Int,
+        isTyping: Boolean
     ) {
         @Suppress("ConvertTwoComparisonsToRangeCheck")
-        if (oldScrollY > 0 && scrollY > oldScrollY && extendedFab.isShown) {
+        if (isTyping || (oldScrollY > 0 && scrollY > oldScrollY && extendedFab.isShown)) {
             setExtendedFabVisibility(extendedFab, false)
         } else if (scrollY < oldScrollY && !extendedFab.isShown) {
             setExtendedFabVisibility(extendedFab, true)
